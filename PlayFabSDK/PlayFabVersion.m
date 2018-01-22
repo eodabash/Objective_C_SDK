1af37e594656a0f9bae26896fe59533e2fa89c4d
PlayFabJenkinsBot <playfabjenkinsbot@github.com>
2018-01-22 21:23:38 +0000

    https://api.playfab.com/releaseNotes/#180122

--- PlayFabSDK/PlayFabVersion.m
+++ PlayFabSDK/PlayFabVersion.m
@@ -1,5 +1,5 @@
 
 #include "PlayFabVersion.h"
 
-NSString * const sdkVersion = @"0.0.171127";
-NSString * const versionString = @"ObjCSDK-0.0.171127";
+NSString * const sdkVersion = @"0.0.180122";
+NSString * const versionString = @"ObjCSDK-0.0.180122";
